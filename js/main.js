c66afa895f3871aa0bd8c93b73f1178ccc29521a
Aleksandr Mikhailov <alex92ailo@gmail.com>
2017-04-07 23:02:24 +0300

    Работа над анимацией текста

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+ onScrollInit( $('.os-animation') );
+ onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
+
+});
+
+function onScrollInit( items, trigger ) {
+  items.each( function() {
+    var osElement = $(this),
+        osAnimationClass = osElement.attr('data-os-animation'),
+        osAnimationDelay = osElement.attr('data-os-animation-delay');
+      
+        osElement.css({
+          '-webkit-animation-delay':  osAnimationDelay,
+          '-moz-animation-delay':     osAnimationDelay,
+          'animation-delay':          osAnimationDelay
+        });
+
+        var osTrigger = ( trigger ) ? trigger : osElement;
+        
+        osTrigger.waypoint(function() {
+          osElement.addClass('animated').addClass(osAnimationClass);
+          },{
+              triggerOnce: true,
+              offset: '90%'
+        });
+  });
+}
