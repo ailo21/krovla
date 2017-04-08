b52c483113a3877ef4919b223d24074ac537575e
a-mikhailov iserv <aleksandr-mikhailov@it-serv.ru>
2017-04-08 17:01:11 +0300

    Эксперименты на работе

--- js/main.js
+++ js/main.js
@@ -1,9 +1,45 @@
 $(document).ready(function () {
 
- onScrollInit( $('.os-animation') );
+    $(window).scroll(function () {
+        ScrollsPage();
+        //onScrollInit($('.os-animation'));
+    })
+
+
+
+    
+ onScrollInit($('.os-animation'));
  onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
 
 });
+
+function ScrollsPage() {
+
+    //if ($('.os-animation').length > 0) {
+    //    $('.os-animation').each(function () {
+    //        var $obj = $(this);
+    //        var TopHeight = $obj.offset().top-250;
+    //        var bo = $("body").scrollTop();
+    //        if (bo > TopHeight) {
+    //            //onScrollInit($obj);
+    //        }
+    //    });
+    //}
+
+
+
+    //if ($('.os-animation').length > 0) {
+    //    $('.os-animation').each(function () {
+    //        var $obj = $(this);
+    //        var TopHeight = $obj.offset().top;
+    //        var bo = $("body").scrollTop();
+    //        if (bo > TopHeight) {
+    //            onScrollInit($obj);
+    //        }
+    //    });
+    //}
+}
+
 
 function onScrollInit( items, trigger ) {
   items.each( function() {
