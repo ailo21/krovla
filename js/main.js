870bd8df9d2598e6c4bd352ca9b707e809d7e24c
Aleksandr Mikhailov <alex92ailo@gmail.com>
2017-04-11 22:02:17 +0300

    совладал с анимацией

--- js/main.js
+++ js/main.js
@@ -1,65 +1,15 @@
 $(document).ready(function () {
 
-    $(window).scroll(function () {
-        ScrollsPage();
-        //onScrollInit($('.os-animation'));
-    })
-
-
-
-    
- onScrollInit($('.os-animation'));
- onScrollInit( $('.staggered-animation'), $('.staggered-animation-container') );
+    $('.start__anim').viewportChecker({      
+      callbackFunction: function(elem, action){
+          Anim(elem.attr('data-typeanim'),elem);
+      }
+    });  
 
 });
 
-function ScrollsPage() {
-
-    //if ($('.os-animation').length > 0) {
-    //    $('.os-animation').each(function () {
-    //        var $obj = $(this);
-    //        var TopHeight = $obj.offset().top-250;
-    //        var bo = $("body").scrollTop();
-    //        if (bo > TopHeight) {
-    //            //onScrollInit($obj);
-    //        }
-    //    });
-    //}
-
-
-
-    //if ($('.os-animation').length > 0) {
-    //    $('.os-animation').each(function () {
-    //        var $obj = $(this);
-    //        var TopHeight = $obj.offset().top;
-    //        var bo = $("body").scrollTop();
-    //        if (bo > TopHeight) {
-    //            onScrollInit($obj);
-    //        }
-    //    });
-    //}
-}
-
-
-function onScrollInit( items, trigger ) {
-  items.each( function() {
-    var osElement = $(this),
-        osAnimationClass = osElement.attr('data-os-animation'),
-        osAnimationDelay = osElement.attr('data-os-animation-delay');
-      
-        osElement.css({
-          '-webkit-animation-delay':  osAnimationDelay,
-          '-moz-animation-delay':     osAnimationDelay,
-          'animation-delay':          osAnimationDelay
-        });
-
-        var osTrigger = ( trigger ) ? trigger : osElement;
-        
-        osTrigger.waypoint(function() {
-          osElement.addClass('animated').addClass(osAnimationClass);
-          },{
-              triggerOnce: true,
-              offset: '90%'
-        });
-  });
-}
+  function Anim(type, obj) {
+    obj.removeClass().addClass(type + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass();
+    });
+  };
